d0f89a756673acdfc30ca1a7d1d7339027b6d20a
wangboy <bjutwangboy@gmail.com>
2016-10-25 18:38:06 +0800

    add some gametype

---
+++ core/src/me/game/act/ActGame.kt
@@ -0,0 +1,13 @@
+package me.game.act
+
+import com.badlogic.gdx.Game
+
+/**
+ * Created by wangbo on 2016/10/25.
+ */
+class ActGame : Game() {
+    override fun create() {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}

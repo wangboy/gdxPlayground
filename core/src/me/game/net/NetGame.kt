37ddbecd788e5dde7dd8434fd866630be43facdf
wangboy <bjutwangboy@gmail.com>
2016-10-26 10:33:04 +0800

    add net game main

---
+++ core/src/me/game/net/NetGame.kt
@@ -0,0 +1,13 @@
+package me.game.net
+
+import com.badlogic.gdx.Game
+
+/**
+ * Created by wangbo on 2016/10/26.
+ */
+class NetGame :Game() {
+    override fun create() {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
